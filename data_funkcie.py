7e11c8d89ebb6d734c7b53a2e0e630933beeb557
Michal Kaukič <mike@feelmath.eu>
2024-02-12 18:47:22 +0100

    Added 07_, 08_, cleaned final

--- data_funkcie.py
+++ data_funkcie.py
@@ -3,3 +3,5 @@ import plotly.express as px
 import numpy as np
 
 # Tu budeme pridavat funkcie pre spracovanie dat a kreslenie
+
+df = pl.read_parquet('data/nyc_taxi310k.parq')
--- final/data_funkcie.py
+++ final/data_funkcie.py
@@ -6,6 +6,7 @@ import numpy as np
 
 # z NB 03_
 
+df = pl.read_parquet('data/nyc_taxi310k.parq')
 
 def monthly_frame(frm, day=True):      # nazvy stplcov v datafrejme mozu byt aj po slovensky :-)
     groupped = frm.group_by(pl.col('pick_dt').dt.day()) if day else\
@@ -44,6 +45,7 @@ def week_plot(frm):
 
 # z NB 04_
 
+
 def daily_frame(frm):   # vyvolame s df ako frm
     df_days = df.group_by(pl.col('pick_dt').dt.day().alias('pick_day'), 
                           pl.col('pick_dt').dt.hour().alias('pick_hour'))\
@@ -53,8 +55,9 @@ def daily_frame(frm):   # vyvolame s df ako frm
     return df_days
 
 
-def daily_plot(frm, day): # frm tu bude povyssia df_days
-    frm_day = frm.filter(pl.col('pick_day') == day).sort(by='pick_hour')
+def daily_plot(day): # frm tu bude povyssia df_days
+    frm_d = daily_frame(df)
+    frm_day = frm_d.filter(pl.col('pick_day') == day).sort(by='pick_hour')
     pass_fares_plot = px.bar(frm_day, x='pick_hour', y=['pass_count', 'fares_count'], barmode='group',
                              labels={'pick_hour': 'Hodina', 'value': 'Hodnoty', 'variable': 'Premenná'})
     pass_fares_plot.update_layout(xaxis=dict(tickmode='array', tickvals=list(range(24))))
@@ -69,9 +72,10 @@ def dt_frame(frm): # frm bude povodna df
     return df_dt
 
 
-def plot_histo(frm, col, rozsah, nbins=20):
+def plot_histo(col, rozsah, nbins=20):
     xlabel = 'Vzdialenosť (míle)' if col == 'distance' else 'Čas jazdy'
-    y, x = np.histogram(frm[col], range=rozsah, bins=nbins)
+    frm_dt = dt_frame(df)
+    y, x = np.histogram(frm_dt[col], range=rozsah, bins=nbins)
     x = (x[0:-1] + x[1:]) / 2  # stredy intervalov, nie konce
     df_hist = pl.DataFrame({'x': x, 'y': y})  # pomocna frejma
     return px.bar(df_hist, x='x', y='y', barmode='group', 
@@ -81,10 +85,17 @@ def plot_histo(frm, col, rozsah, nbins=20):
 
 def map_plot(frm, day, hour, pick=True):   # pick=False znamena, ze drop
     col_prefix = 'pick_' if pick else 'drop_'
+    what = 'nástupy' if pick else 'výstupy'
     df_dh = frm.filter((pl.col(f'{col_prefix}dt').dt.day() == day) & (pl.col(f'{col_prefix}dt').dt.hour() == hour))
     mapa = px.scatter_mapbox(df_dh, lat=f'{col_prefix}lat', lon=f'{col_prefix}lon', mapbox_style="open-street-map", 
                          zoom=10, color_discrete_sequence=["darkblue"], width=500, height=500, opacity=0.3,
-                         title=f'Počet jázd: {df_dh.shape[0]}')
+                         title=f'Počet jázd, {what}: {df_dh.shape[0]}')
     mapa.update_traces(marker={"size": 4})
     mapa.update_layout(margin={'t': 30, 'b': 10}, hovermode=False)
     return mapa
+
+def pick_plot(day, hour):
+    return map_plot(df, day, hour, pick=True)
+
+def drop_plot(day, hour):
+    return map_plot(df, day, hour, pick=False)
