52095635657bb122decd84e3c7c78c66838e1ef0
Michal Kaukič <mike@feelmath.eu>
2024-02-20 11:55:29 +0100

    Added taxi_marimo_map, final/view_functions

---
+++ final/view_functions.py
@@ -0,0 +1,63 @@
+import marimo as mo
+import numpy as np
+import polars as pl
+import plotly.express as px
+
+
+def daily_frame(frm, pick=True):   # vyvolame s df ako frm
+    col_prefix='pick_' if pick else 'drop_'
+    df_days = frm.group_by(pl.col(f'{col_prefix}dt').dt.day().alias(f'{col_prefix}day'), 
+                           pl.col(f'{col_prefix}dt').dt.hour().alias(f'{col_prefix}hour'))\
+                          .agg([pl.col('passengers').sum().alias('pass_count'),
+                                pl.col('fare').count().alias('fares_count'),
+                                pl.col('fare').sum().alias('total_fare')])
+    return df_days
+
+
+def _daily_plot(frm, day, pick=True): # frm tu bude povyssia df_days, day bude z radio day_choose
+    frm_d = daily_frame(frm, pick=pick)
+    col_prefix='pick_' if pick else 'drop_'
+    frm_day = frm_d.filter(pl.col(f'{col_prefix}day') == day).sort(by=f'{col_prefix}hour')
+    pass_fares_plot = px.bar(frm_day, x=f'{col_prefix}hour', y=['pass_count', 'fares_count'], barmode='group', height=350,
+                             labels={f'{col_prefix}hour': 'Hodina', 'value': 'Hodnoty', 'variable': 'Premenná'})
+    pass_fares_plot.update_layout(xaxis=dict(tickmode='array', tickvals=list(range(24))))
+    return pass_fares_plot
+
+
+def _view_map(df, dir, dcv, hcv):
+    is_pick = (dir == 'Nástup')
+    column = 'pick_dt' if is_pick else 'drop_dt'
+    df_filtered = df.filter((df[column].dt.day() == dcv) & (df[column].dt.hour() == hcv))
+    col_lat = 'pick_lat' if is_pick else 'drop_lat'
+    col_lon = 'pick_lon' if is_pick else 'drop_lon'
+    lat, lon = df_filtered[col_lat], df_filtered[col_lon]
+    center_lat, center_lon = [lat.mean(), lon.mean()] if len(lat) else meanloc
+    fig = px.scatter_mapbox(df_filtered, lat=col_lat, lon=col_lon,
+                            color_discrete_sequence=["green"],
+                            mapbox_style="open-street-map", zoom=10, width=750, height=500)
+    fig.update_traces(marker={"size": 4})
+    fig.update_layout(margin={'t': 25}, hovermode=False)
+    return mo.ui.plotly(fig)
+
+
+def _view_selection(df, mapplot, selval, mdv):
+    _local_data = ((mapplot.ranges != {}) and selval)
+    if _local_data:
+        mapranges = mapplot.ranges['mapbox']
+        lon_min, lat_max = mapranges[0]
+        lon_max, lat_min = mapranges[1]
+        df_ranges_pick = df.filter((lat_min < pl.col('pick_lat')) & (pl.col('pick_lat') < lat_max) &
+                                   (lon_min < pl.col('pick_lon')) & (pl.col('pick_lon') < lon_max) &
+                                   (pl.col('pick_dt').dt.day() == mdv))
+        df_ranges_drop = df.filter((lat_min < pl.col('drop_lat')) & (pl.col('drop_lat') < lat_max) &
+                                   (lon_min < pl.col('drop_lon')) & (pl.col('drop_lon') < lon_max) &
+                                   (pl.col('drop_dt').dt.day() == mdv))
+        pick_rows = (df_ranges_pick.rows() != [])
+        drop_rows = (df_ranges_drop.rows() != [])
+        no_rides = mo.md("## Nikto nejazdil")
+        pick_plot = _daily_plot(df_ranges_pick, mdv, pick=True) if pick_rows else no_rides
+        drop_plot = _daily_plot(df_ranges_drop, mdv, pick=False) if drop_rows else no_rides
+    else: 
+        no_plot = mo.md('## Źiadny výber, alebo výber nie je povolený')
+        return no_plot
+    return mo.vstack([pick_plot, drop_plot])
---
+++ taxi_marimo_map.py
@@ -0,0 +1,77 @@
+import marimo
+
+__generated_with = "0.2.5"
+app = marimo.App(width="full")
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+@app.cell
+def __():
+    import polars as pl
+    from final.view_functions import _view_map, _view_selection
+    return pl,
+
+
+@app.cell
+def __(pl):
+    _dflocal = 'data/nyc_taxi310k.parq'
+    df = pl.read_parquet(_dflocal)
+    meanloc = [df['pick_lat'].mean(), df['pick_lon'].mean()]
+    return df, meanloc
+
+
+@app.cell
+def __(mo):
+    day_choose = mo.ui.slider(start=1, stop=31, value=14, debounce=True, label='Deň')
+    hour_choose = mo.ui.slider(start=0, stop=23, value=11, debounce=True, label='Hodina')
+    direction = mo.ui.radio(options=['Nástup','Výstup'], value='Nástup', label='Smer', inline=True)
+    map_selection = mo.ui.checkbox(label='Umožniť výber')
+    map_day_choose = mo.ui.slider(start=1, stop=31, value=14, debounce=True, label='Deň pre výber')
+    return day_choose, direction, hour_choose, map_day_choose, map_selection
+
+
+@app.cell
+def __(day_choose, df, direction, hour_choose):
+    mapplot = _view_map(df, direction.value, day_choose.value, hour_choose.value)
+    return mapplot,
+
+
+@app.cell
+def __(
+    day_choose,
+    df,
+    direction,
+    hour_choose,
+    map_day_choose,
+    map_selection,
+    mapplot,
+    mo,
+):
+    _main_title = mo.md(
+        """
+        # Taxi v New Yorku
+        ### Dáta z januára 2015, výber 310000 záznamov.""")
+
+    _local_data =(mapplot.ranges != {}) and map_selection.value
+    _local_plots = _view_selection(df, mapplot, map_selection.value, map_day_choose.value)
+    _sel_info = mo.md(f"Deň: {map_day_choose.value}")
+    _loc_widgets = mo.hstack([map_day_choose, _sel_info], justify='center', widths=[100, 50])
+    _selection_body = mo.vstack([_loc_widgets, _local_plots]) if _local_data else mo.vstack([_local_plots])
+
+    _map_info = mo.md(f"Deň: {day_choose.value}, Hodina: {hour_choose.value}")
+    _maps = mo.vstack([mo.hstack([direction, day_choose, hour_choose, map_selection], justify='center'), 
+                      _map_info, mapplot], align='center')
+    _tabs = mo.ui.tabs({'Polohy na mape': _maps, 'Grafy pre výber': _selection_body})
+
+    app_tabs = mo.vstack([_main_title, _tabs], align='stretch') 
+    app_tabs
+    return app_tabs,
+
+
+if __name__ == "__main__":
+    app.run()
