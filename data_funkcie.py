11937f14beb22688fc75c172d1f502afd93b3503
Michal Kaukič <mike@feelmath.eu>
2024-03-14 08:56:46 +0100

    data_funkcie deleted

--- data_funkcie.py
+++
@@ -1,7 +0,0 @@
-import polars as pl
-import plotly.express as px
-import numpy as np
-
-# Tu budeme pridavat funkcie pre spracovanie dat a kreslenie
-
-df = pl.read_parquet('data/nyc_taxi310k.parq')
